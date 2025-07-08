98b49d12c9ea3da75a54bd353e8643f5319f21bd
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-08 12:57:21 +0300

    Added rover photos path and config file

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import express from "express";
 import axios from "axios";
 
 const NASA_KEY = "26LavwLhD6YneXtJKDW1IhKx8ncitgYLebCyF7zY";
-const rover_body = 'https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=';
+const roverBody = 'https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=';
+
+const roverName = 'curiosity';
+const camera = 'fhaz';
+
+const roverCameraBody = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos`;
+const roverCameraParams = `?sol=1000&camera=${camera}&api_key=${NASA_KEY}`;
 
 const app = express();
 const port = 8000;
@@ -17,7 +23,19 @@ router.get('/test', (req: any, res: any) => res.send('Hello Vlad !'));
 router.get('/rovers', async (req: any, res: any) =>
     {
         try {
-            const resp = await axios.get(rover_body + NASA_KEY);
+            const resp = await axios.get(roverBody + NASA_KEY);
+            res.json(resp.data);
+        } catch (e) {
+            res.status(500).json({'Something went wrong': e});
+        }
+    }
+);
+
+// Fetch images by rover and camera name
+router.get(`/rovers/${roverName}/photos/${camera}`, async (req: any, res: any) =>
+    {
+        try {
+            const resp = await axios.get(roverCameraBody + roverCameraParams);
             res.json(resp.data);
         } catch (e) {
             res.status(500).json({'Something went wrong': e});
